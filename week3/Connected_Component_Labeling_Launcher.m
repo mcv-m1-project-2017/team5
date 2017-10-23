1b11d541629195e5b5d588ebb1194892b17d6490
Pedro Luis Trigueros Mondéjar <Pedro@Pedros-MacBook-Pro.local>
2017-10-23 20:43:54 +0200

    CCL updated

--- week3/Connected_Component_Labeling_Launcher.m
+++ week3/Connected_Component_Labeling_Launcher.m
@@ -15,28 +15,42 @@ if exist (strcat(directory, 'mat' ), 'dir') ~= 7
     mkdir(strcat(directory, 'mat' ));
 end
 
-[sFrequency ,maxSize ,minSize ,formFactor ,fillingRatio,sFrequencyPercent] = SignalCharacteristics('../Dataset/train/');
+[sFrequency ,maxSize ,minSize ,formFactor ,fillingRatio,sFrequencyPercent] = SignalCharacteristics('../DataSet/train/');
 
-
+totMinSize      = 0;
+totMaxSize      = 0;
+totFillingRatio = 0;
+for n=1:size(maxSize,2)
+    totMinSize      = totMinSize + minSize(n);
+    totMaxSize      = totMaxSize + maxSize(n);
+    totFillingRatio = totFillingRatio + fillingRatio(n);
+end
+totFillingRatio = totFillingRatio / size(maxSize,2);
+avgTotMinSize = totMinSize / size(maxSize,2);
+avgTotMaxSize = totMaxSize / size(maxSize,2);
 for i=1:size(files,1)
     % Read mask
-    mask  = imread(strcat(directory, files(i).name(1:size(files(i).name,2)-3), 'png'));
+    mask = imread(strcat(directory, files(i).name(1:size(files(i).name,2)-3), 'png'));
+    CCL  = bwconncomp ( mask );
+    CC   = regionprops( CCL,'basic' );
     figure;
     imshow(mask);
-    CC      = bwconncomp ( mask );
-    allAreas = regionprops( CC,'basic' );
-    hold on;
+    hold on
     %Show all areas in image
-    for n=1:size(allAreas,1)
-        rectangle('Position',allAreas(n).BoundingBox,'EdgeColor','g','LineWidth',2)
+    for n=1:size(CC,1)
+        rectangle('Position',CC(n).BoundingBox,'EdgeColor','g','LineWidth',2)
     end
-    areas=find([allAreas.Area]<0.004);
+    NotSignal=find( [CC.Area] < avgTotMinSize );
     %Show areas that are less than area average
-    for n=1:size(areas,2)
-        rectangle('Position',allAreas(areas(n)).BoundingBox,'EdgeColor','r','LineWidth',2)
+    for n=1:size(NotSignal,2)
+        rectangle('Position',CC(NotSignal(n)).BoundingBox,'EdgeColor','r','LineWidth',2)
     end
-    for n=1:size(areas,2)
-        d=round(allAreas(areas(n)).BoundingBox);
-        mask(d(2):d(2)+d(4),d(1):d(1)+d(3))=0;
+    NotSignal1=find( [CC.Area] > avgTotMaxSize );
+    for n=1:size(NotSignal1,2)
+        rectangle('Position',CC(NotSignal1(n)).BoundingBox,'EdgeColor','b','LineWidth',2)
     end
+    %for n=1:size(areas,2)
+    %    d=round(allAreas(areas(n)).BoundingBox);
+    %    mask(d(2):d(2)+d(4),d(1):d(1)+d(3))=0;
+    %end
 end

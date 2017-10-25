06867254dd06026e771af57eba9f7b6b0340f870
Pedro Luis Trigueros Mondéjar <Pedro@Pedros-MacBook-Pro.local>
2017-10-25 20:32:46 +0200

    task1/week3 updated

--- week3/Connected_Component_Labeling_Launcher.m
+++ week3/Connected_Component_Labeling_Launcher.m
@@ -6,6 +6,7 @@ directory = '../DataSet/train/validation_split/YcbCrAndHSV_mask/4_3/';
 if exist(directory, 'dir') ~= 7
     error('Directory not found');
 end
+
 files = ListFiles(directory);
 if size(files,1) == 0
     error('Directory is empty');
@@ -14,9 +15,10 @@ end
 if exist (strcat(directory, 'mat' ), 'dir') ~= 7
     mkdir(strcat(directory, 'mat' ));
 end
-
+%Get values from dataset with week1 function
 [sFrequency ,maxSize ,minSize ,formFactor ,fillingRatio,sFrequencyPercent] = SignalCharacteristics('../DataSet/train/');
 
+%Compute values averages
 totMinSize      = 0;
 totMaxSize      = 0;
 totFillingRatio = 0;
@@ -26,8 +28,9 @@ for n=1:size(maxSize,2)
     totFillingRatio = totFillingRatio + fillingRatio(n);
 end
 totFillingRatio = totFillingRatio / size(maxSize,2);
-avgTotMinSize = totMinSize / size(maxSize,2);
-avgTotMaxSize = totMaxSize / size(maxSize,2);
+avgTotMinSize   = totMinSize      / size(maxSize,2);
+avgTotMaxSize   = totMaxSize      / size(maxSize,2);
+
 for i=1:size(files,1)
     % Read mask
     mask = imread(strcat(directory, files(i).name(1:size(files(i).name,2)-3), 'png'));
@@ -36,21 +39,10 @@ for i=1:size(files,1)
     figure;
     imshow(mask);
     hold on
-    %Show all areas in image
-    for n=1:size(CC,1)
-        rectangle('Position',CC(n).BoundingBox,'EdgeColor','g','LineWidth',2)
+    %Delete areas that are greater than area average or are less than area average
+    Signals = CC([CC.Area] >= avgTotMinSize & [CC.Area] <= avgTotMaxSize);
+    %Show areas in image
+    for n=1:size(Signals,1)
+        rectangle('Position',Signals(n).BoundingBox,'EdgeColor','g','LineWidth',2)
     end
-    NotSignal=find( [CC.Area] < avgTotMinSize );
-    %Show areas that are less than area average
-    for n=1:size(NotSignal,2)
-        rectangle('Position',CC(NotSignal(n)).BoundingBox,'EdgeColor','r','LineWidth',2)
-    end
-    NotSignal1=find( [CC.Area] > avgTotMaxSize );
-    for n=1:size(NotSignal1,2)
-        rectangle('Position',CC(NotSignal1(n)).BoundingBox,'EdgeColor','b','LineWidth',2)
-    end
-    %for n=1:size(areas,2)
-    %    d=round(allAreas(areas(n)).BoundingBox);
-    %    mask(d(2):d(2)+d(4),d(1):d(1)+d(3))=0;
-    %end
 end

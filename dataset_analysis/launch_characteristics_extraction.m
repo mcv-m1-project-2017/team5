073a84ce641a6f72ee0f64457e8942373b52ff9c
Pedro Luis Trigueros Mondéjar <Pedro@Pedros-MacBook-Pro.local>
2017-11-03 18:06:55 +0100

    Remove duplicated directory

--- dataset_analysis/launch_characteristics_extraction.m
+++
@@ -1,4 +0,0 @@
-clear;
-addpath(genpath('..'))
-[sFrequency ,maxSize ,minSize ,formFactor ,fillingRatio,sFrequencyPercent] = SignalCharacteristics('../Dataset/train');
-SplitDataset( sFrequency ,maxSize ,minSize ,formFactor ,fillingRatio ,'../Dataset/train' )

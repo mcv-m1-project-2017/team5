69e144755367b10fee1db083db4a2d228ce3dffc
Pedro Luis Trigueros Mondéjar <Pedro@Pedros-MacBook-Pro.local>
2017-11-04 12:10:54 +0100

    get_templates updated

--- week4/launchGetTemplates.m
+++ week4/launchGetTemplates.m
@@ -1,4 +1,4 @@
 clear;clc; close all;
-addpath(genpath('../..'))
+addpath(genpath('..'));
 %Launch getTemplates
-[Templates] = get_templates('../DataSet/train/train_split',true,true);
+[Templates] = get_templates('../DataSet/train/train_split',true,false);
